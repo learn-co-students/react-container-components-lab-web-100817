b4388c5557135fc45e9933207be0d863a179c256
github username <github email address>
2017-12-13 12:07:49 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieReviews = (props) => {
+  // how come brackets make the test fail but parentheses work?
+
+  return <div className="review-list">
+    {props.reviews.map(review =>
+    <p className="review"> {review.headline}</p>)}
+    </div>
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,62 @@
 // Code SearchableMovieReviewsContainer Here
+
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews'
+
+//
+
+
+class SearchableMovieReviewsContainer extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+    fetchReviews = event => {
+      event.preventDefault()
+      const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}&api-key=97eb60d840d042dbb12ef492a8abe754`
+      fetch(URL).then(response => response.json())
+      .then((reviews => this.setState({
+        reviews: reviews.results
+      })
+      ));
+    }
+
+    handleChange = (event) => {
+      this.setState({searchTerm: event.target.value})
+    }
+
+    render(){
+       return (
+       <div className="searchable-movie-reviews">
+       <form onSubmit={this.fetchReviews}>
+         <input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
+         <input type= "submit"></input>
+       </form>
+       <MovieReviews reviews={this.state.reviews}/>
+       </div>
+       )
+     }
+
+}
+
+export default SearchableMovieReviewsContainer;
+
+//   componentWillMount() {
+//     fetch(URL).then(response => response.json())
+//     .then((reviews => this.setState({reviews: reviews.results})));
+//   }
+//   render(){
+//     return (
+//     <div className="latest-movie-reviews">
+//     <MovieReviews reviews={this.state.reviews}/>
+//     </div>
+//     )
+//   }
+// }
+// export default LatestMovieReviewsContainer;
