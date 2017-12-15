a11c5957abb2ad328fb65b86005c0b655fb78215
github username <github email address>
2017-12-15 09:10:42 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
 // Code MovieReviews Here
+import React from 'react'
+import Review from './Review'
+
+class MovieReviews extends React.Component {
+
+  render() {
+
+    const reviews = this.props.reviews.map(review => <Review review= {review} />)
+
+    return (
+      <div className="review-list">
+        {reviews}
+      </div>
+    )
+  }
+}
+
+
+export default MovieReviews
